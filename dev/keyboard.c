a425deb8fe73df0ee880e02053c717e63780b1c2
borfus <mrpeteyb@gmail.com>
2018-10-03 22:39:06 -0500

    Added many more release, hold, and press key functions.

--- dev/keyboard.c
+++ dev/keyboard.c
@@ -25,6 +25,17 @@ short convert_to_keycode(char *key)
     return VkKeyScan(converted_key);
 }
 
+int is_non_numeric_shift_symbol(char a)
+{
+    if (a == '~' || a == '_' || a == '+' || a == '{' || 
+        a == '}' || a == '|' || a == ':' || a == '"' || 
+        a == '<' || a == '>' || a == '?')
+    {
+        return 1;
+    }
+    return 0;
+}
+
 void hold_shift()
 {
     setup_keyboard_press();
@@ -37,6 +48,224 @@ void release_shift()
 {
     setup_keyboard_press();
     keyboard_input.ki.wVk = VK_LSHIFT;
+    keyboard_input.ki.dwFlags = KEYEVENTF_KEYUP;
+    send_keyboard_input();
+}
+
+void hold_ctrl()
+{
+    setup_keyboard_press();
+    keyboard_input.ki.wVk = VK_CONTROL;
+    keyboard_input.ki.dwFlags = 0;
+    send_keyboard_input();
+}
+
+void release_ctrl()
+{
+    setup_keyboard_press();
+    keyboard_input.ki.wVk = VK_CONTROL;
+    keyboard_input.ki.dwFlags = KEYEVENTF_KEYUP;
+    send_keyboard_input();
+}
+
+void hold_alt()
+{
+    setup_keyboard_press();
+    keyboard_input.ki.wVk = VK_MENU;
+    keyboard_input.ki.dwFlags = 0;
+    send_keyboard_input();
+}
+
+void release_alt()
+{
+    setup_keyboard_press();
+    keyboard_input.ki.wVk = VK_MENU;
+    keyboard_input.ki.dwFlags = KEYEVENTF_KEYUP;
+    send_keyboard_input();
+}
+
+void hold_capslock()
+{
+    setup_keyboard_press();
+    keyboard_input.ki.wVk = VK_CAPITAL;
+    keyboard_input.ki.dwFlags = 0;
+    send_keyboard_input();
+}
+
+void release_capslock()
+{
+    setup_keyboard_press();
+    keyboard_input.ki.wVk = VK_CAPITAL;
+    keyboard_input.ki.dwFlags = KEYEVENTF_KEYUP;
+    send_keyboard_input();
+}
+
+void hold_tab()
+{
+    setup_keyboard_press();
+    keyboard_input.ki.wVk = VK_TAB;
+    keyboard_input.ki.dwFlags = 0;
+    send_keyboard_input();
+}
+
+void release_tab()
+{
+    setup_keyboard_press();
+    keyboard_input.ki.wVk = VK_TAB;
+    keyboard_input.ki.dwFlags = KEYEVENTF_KEYUP;
+    send_keyboard_input();
+}
+
+void hold_backspace()
+{
+    setup_keyboard_press();
+    keyboard_input.ki.wVk = VK_BACK;
+    keyboard_input.ki.dwFlags = 0;
+    send_keyboard_input();
+}
+
+void release_backspace()
+{
+    setup_keyboard_press();
+    keyboard_input.ki.wVk = VK_BACK;
+    keyboard_input.ki.dwFlags = KEYEVENTF_KEYUP;
+    send_keyboard_input();
+}
+
+void hold_space()
+{
+    setup_keyboard_press();
+    keyboard_input.ki.wVk = VK_SPACE;
+    keyboard_input.ki.dwFlags = 0;
+    send_keyboard_input();
+}
+
+void release_space()
+{
+    setup_keyboard_press();
+    keyboard_input.ki.wVk = VK_SPACE;
+    keyboard_input.ki.dwFlags = KEYEVENTF_KEYUP;
+    send_keyboard_input();
+}
+
+void hold_up()
+{
+    setup_keyboard_press();
+    keyboard_input.ki.wVk = VK_UP;
+    keyboard_input.ki.dwFlags = 0;
+    send_keyboard_input();
+}
+
+void release_up()
+{
+    setup_keyboard_press();
+    keyboard_input.ki.wVk = VK_UP;
+    keyboard_input.ki.dwFlags = KEYEVENTF_KEYUP;
+    send_keyboard_input();
+}
+
+void hold_down()
+{
+    setup_keyboard_press();
+    keyboard_input.ki.wVk = VK_DOWN;
+    keyboard_input.ki.dwFlags = 0;
+    send_keyboard_input();
+}
+
+void release_down()
+{
+    setup_keyboard_press();
+    keyboard_input.ki.wVk = VK_DOWN;
+    keyboard_input.ki.dwFlags = KEYEVENTF_KEYUP;
+    send_keyboard_input();
+}
+
+void hold_left()
+{
+    setup_keyboard_press();
+    keyboard_input.ki.wVk = VK_LEFT;
+    keyboard_input.ki.dwFlags = 0;
+    send_keyboard_input();
+}
+
+void release_left()
+{
+    setup_keyboard_press();
+    keyboard_input.ki.wVk = VK_LEFT;
+    keyboard_input.ki.dwFlags = KEYEVENTF_KEYUP;
+    send_keyboard_input();
+}
+
+void hold_right()
+{
+    setup_keyboard_press();
+    keyboard_input.ki.wVk = VK_RIGHT;
+    keyboard_input.ki.dwFlags = 0;
+    send_keyboard_input();
+}
+
+void release_right()
+{
+    setup_keyboard_press();
+    keyboard_input.ki.wVk = VK_RIGHT;
+    keyboard_input.ki.dwFlags = KEYEVENTF_KEYUP;
+    send_keyboard_input();
+}
+
+void press_capslock()
+{
+    hold_capslock();
+    release_capslock();
+}
+
+void press_tab()
+{
+    hold_tab();
+    release_tab();
+}
+
+void press_backspace()
+{
+    hold_backspace();
+    release_backspace();
+}
+
+void press_space()
+{
+    hold_space();
+    release_space();
+}
+
+void press_up()
+{
+    hold_up();
+    release_up();
+}
+
+void press_down()
+{
+    hold_down();
+    release_down();
+}
+
+void press_left()
+{
+    hold_left();
+    release_left();
+}
+
+void press_right()
+{
+    hold_right();
+    release_right();
+}
+
+void press_escape()
+{
+    setup_keyboard_press();
+    keyboard_input.ki.wVk = VK_ESCAPE;
+    send_keyboard_input();
+
     keyboard_input.ki.dwFlags = KEYEVENTF_KEYUP;
     send_keyboard_input();
 }
@@ -59,17 +288,6 @@ void press_key(char key)
 
     keyboard_input.ki.dwFlags = KEYEVENTF_KEYUP;
     send_keyboard_input();
-}
-
-int is_non_numeric_shift_symbol(char a)
-{
-    if (a == '~' || a == '_' || a == '+' || a == '{' || 
-        a == '}' || a == '|' || a == ':' || a == '"' || 
-        a == '<' || a == '>' || a == '?')
-    {
-        return 1;
-    }
-    return 0;
 }
 
 void type(char *words)
